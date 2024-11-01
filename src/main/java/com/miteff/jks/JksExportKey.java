af7245ea645e8e8bd5ea39be62c1dc28fe4223af
joro88 <mitev.joro@gmail.com>
2024-11-01 06:37:04 +0200

    spaces changed to tabs

--- src/main/java/com/miteff/jks/JksExportKey.java
+++ src/main/java/com/miteff/jks/JksExportKey.java
@@ -5,12 +5,12 @@
  *
  * How to export the private key from keystore?
  * Does keytool not have an option to do so?
- * Georgi Mitev: Some patches applied to work 
+ * Mitev: Some patches applied to work 
  * Alexey Zilber: Ported to work with Base64Coder: http://www.source-code.biz/snippets/java/2.htm
  
  @see <a href = "http://miteff.com" > miteff.com </a> for more information
  
- * @author Georgi Mitev: Some patches applied to work 
+ * @author Mitev: Some patches applied to work 
  * @author Alexey Zilber: Ported to work with Base64Coder: http://www.source-code.biz/snippets/java/2.htm
  */
 package com.miteff.jks;
@@ -25,7 +25,7 @@ class JksExportKey {
     public static void main(String args[]) throws Exception{
 		if (args.length < 2) {
 			// Yes I know it is not good to pass password in console but 
-            // it is a quick-n-dirty fix to export from a keystore to pkcs12
+			// it is a quick-n-dirty fix to export from a keystore to pkcs12
 			System.err.println("Usage: java -jar jksExportKey-x.y.jar <keystore> <alias> <password>");
 			System.exit(1);
 		}
@@ -34,37 +34,36 @@ class JksExportKey {
     }
 
     public void doit( String fileName, String aliasName, String pass ) throws Exception {
+		KeyStore ks = KeyStore.getInstance("JKS");
 
-	KeyStore ks = KeyStore.getInstance("JKS");
+		char[] passPhrase = pass.toCharArray();
+		//BASE64Encoder myB64 = new BASE64Encoder();
 
-	char[] passPhrase = pass.toCharArray();
-	//BASE64Encoder myB64 = new BASE64Encoder();
+		File certificateFile = new File(fileName);
+		ks.load(new FileInputStream(certificateFile), passPhrase);
 
-	File certificateFile = new File(fileName);
-	ks.load(new FileInputStream(certificateFile), passPhrase);
-
-	KeyPair kp = getPrivateKey(ks, aliasName, passPhrase);
+		KeyPair kp = getPrivateKey(ks, aliasName, passPhrase);
 		
-	PrivateKey privKey = kp.getPrivate();
+		PrivateKey privKey = kp.getPrivate();
 
-	// Georgi Mitev: patch for openssl problem, 12 lines
-	char[] b64 = Base64Coder.encode(privKey.getEncoded());
-	byte[] b64b = new byte[b64.length];
-	for (int c = 0; c < b64.length; c++) {
-	  b64b[c] = (byte) b64[c];
+		// patch for openssl problem, 12 lines
+		char[] b64 = Base64Coder.encode(privKey.getEncoded());
+		byte[] b64b = new byte[b64.length];
+		for (int c = 0; c < b64.length; c++) {
+			b64b[c] = (byte) b64[c];
+		}
+
+		System.out.println("-----BEGIN PRIVATE KEY-----");
+		for (int c = 0; c < b64b.length; c+=64) {
+	    	int l = Math.min(64, b64b.length - c);
+		    System.out.write(b64b, c, l);
+		    System.out.println();
+		}
+		System.out.println("-----END PRIVATE KEY-----");
+
 	}
 
-	System.out.println("-----BEGIN PRIVATE KEY-----");
-	for (int c = 0; c < b64b.length; c+=64) {
-	    int l = Math.min(64, b64b.length - c);
-	    System.out.write(b64b, c, l);
-	    System.out.println();
-	}
-	System.out.println("-----END PRIVATE KEY-----");
-
-	}
-
-   public KeyPair getPrivateKey(KeyStore keystore, String alias, char[] password) {
+    public KeyPair getPrivateKey(KeyStore keystore, String alias, char[] password) {
         try {
             // Get private key
             Key key = keystore.getKey(alias, password);
